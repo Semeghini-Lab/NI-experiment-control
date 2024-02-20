c45f3f844beafb7e464e832cf3f77c11f0d1e635
ps-cd <45522676+ps-cd@users.noreply.github.com>
2024-02-20 17:48:54 -0500

    [Static ref clk] Inactive card as ref clk scr

--- niexpctrl_backend/src/connect_terms.rs
+++ niexpctrl_backend/src/connect_terms.rs
@@ -2,6 +2,6 @@ mod nidaqmx;
 use nidaqmx::*;
 
 fn main() {
-    connect_terms("/Dev2/10MHzRefClock", "/Dev2/RTSI7");
+    connect_terms("/Dev1/10MHzRefClock", "/Dev1/RTSI7");
     println!("Connected terminals");
 }
